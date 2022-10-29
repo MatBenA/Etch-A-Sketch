536d3bf32397339cd23548c4252a1bd653a08e05
Matías <crosserain@gmail.com>
2022-10-29 10:31:30 -0300

    256 cells were added with javascript using an array.

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const container = document.querySelector(".container");
 const cell = document.createElement("div");
 cell.classList.add("cell");
 container.appendChild(cell);
-
 const cells = [];
 
+for(i = 0; i < 256; i++){
+    cells.push(document.createElement("div"));
+    cells[i].classList.add("cell");
+    container.appendChild(cells[i]);
+}
 //256 cells
