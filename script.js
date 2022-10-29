8867ccdf4be37d20634fcc60c8438b6bdc3df5b8
Matías <crosserain@gmail.com>
2022-10-29 10:37:48 -0300

    unuseful code deleted. cells style changed.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const container = document.querySelector(".container");
-const cell = document.createElement("div");
-cell.classList.add("cell");
-container.appendChild(cell);
-const cells = [];
 
+const cells = [];
 for(i = 0; i < 256; i++){
     cells.push(document.createElement("div"));
     cells[i].classList.add("cell");
