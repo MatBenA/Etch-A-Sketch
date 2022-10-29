e86a898fb9d390f3a6daedfe9c378e4f8094825c
Matías <crosserain@gmail.com>
2022-10-29 10:06:54 -0300

    style added. The container div was created and a cell was added to it with JavaScript.

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+const cell = document.createElement("div");
+cell.classList.add("cell");
+container.appendChild(cell);
+
+const cells = [];
+
+//256 cells
