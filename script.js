74426e98963a137cc47eda9e969d605ac0a5fb33
Matías <crosserain@gmail.com>
2022-10-29 18:54:29 -0300

    hover effect added so that the grid divs change color when your  mouse passes over them, leaving a (pixelated) trail through your  grid like a pen would.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const content = document.querySelector(".content");
 
+/* content.addEventListener('mouseover', () => {
+    content.style.backgroundColor = "red";
+});
+
+content.addEventListener('mouseout', () => {
+    content.style.backgroundColor = "beige";
+}); */
 
 
 const cells = [];
@@ -7,6 +14,18 @@ for(i = 0; i < 256; i++){
     cells.push(document.createElement("div"));
     cells[i].classList.add("cell");
     content.appendChild(cells[i]);
+
+    colorCell(i);
+}
+
+function colorCell(cellNumber){
+    cells[cellNumber].addEventListener("mouseover", () =>{
+        cells[cellNumber].style.backgroundColor = "red";
+    })
+
+/*     cells[cellNumber].addEventListener("mouseout", () =>{
+        cells[cellNumber].style.backgroundColor = "beige";
+    }) */
 }
 
 //256 cells
