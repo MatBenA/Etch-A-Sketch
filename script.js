37289692b061f4ef768f84ac082c10934e1ea598
Matías <crosserain@gmail.com>
2022-10-29 17:28:50 -0300

    The conteiner div is now a responsive square

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const container = document.querySelector(".container");
+const content = document.querySelector(".content");
+
+
 
 const cells = [];
 for(i = 0; i < 256; i++){
     cells.push(document.createElement("div"));
     cells[i].classList.add("cell");
-    container.appendChild(cells[i]);
+    content.appendChild(cells[i]);
 }
+
 //256 cells
