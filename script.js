8bec76a95513826fe2e07b7fa1e9a7920d7ba67a
Matías <crosserain@gmail.com>
2022-10-30 11:08:42 -0300

    Now you have to press over the container, after that the cells will start painting.

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 const content = document.querySelector(".content");
-
-/* content.addEventListener('mouseover', () => {
-    content.style.backgroundColor = "red";
-});
-
-content.addEventListener('mouseout', () => {
-    content.style.backgroundColor = "beige";
-}); */
-
-
 const cells = [];
+
+//creates all the divs that will go into the painting grid and appends them into the dom
 for(i = 0; i < 256; i++){
     cells.push(document.createElement("div"));
     cells[i].classList.add("cell");
     content.appendChild(cells[i]);
 
-    colorCell(i);
+    //calls the function in charge of painting the grid
+    paint(i);
 }
 
+/* Colors the cells when the mouse is over them*/
 function colorCell(cellNumber){
     cells[cellNumber].addEventListener("mouseover", () =>{
         cells[cellNumber].style.backgroundColor = "red";
     })
+}
 
-/*     cells[cellNumber].addEventListener("mouseout", () =>{
-        cells[cellNumber].style.backgroundColor = "beige";
-    }) */
+/* Checks if a mouse button is being pressed over the conteiner,
+   if thats true then execute the colorCell() function */
+function paint(cellNumber){
+    content.addEventListener("mousedown",() => colorCell(cellNumber));
 }
 
 //256 cells
