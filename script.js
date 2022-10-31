b72acc4acbeae42c318baba18149c23798545993
Matías <crosserain@gmail.com>
2022-10-31 18:24:13 -0300

    Now when you release a the mouse button it stops painting

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const content = document.querySelector(".content");
-const cells = [];
+const container = document.querySelector(".container");
+const cells = []; //array that will contain all the cells
+let mouseDown = false;
 
-//creates all the divs that will go into the painting grid and appends them into the dom
-for(i = 0; i < 256; i++){
-    cells.push(document.createElement("div"));
-    cells[i].classList.add("cell");
-    content.appendChild(cells[i]);
-
-    //calls the function in charge of painting the grid
-    paint(i);
+/* 
+creates all the divs that will go into the painting
+grid and appends them into the dom */
+for (let i = 0; i < 256; i++) {
+  cells.push(document.createElement("div"));
+  container.appendChild(cells[i]);
 }
 
-/* Checks if a mouse button is being pressed over the conteiner,
-   if thats true then execute the colorCell() function */
-function paint(cellNumber){
-    content.addEventListener("mousedown",() => colorCell(cellNumber));
-}
+// This is to know if a mouse button is being pressed over the container
+container.addEventListener("mousedown", () => (mouseDown = true));
+container.addEventListener("mouseup", () => (mouseDown = false));
 
-/* Colors the cells when the mouse is over them*/
-function colorCell(cellNumber){
-    cells[cellNumber].addEventListener("mouseover", () =>{
-        cells[cellNumber].style.backgroundColor = "red";
-    })
+for (let i = 0; i < 256; i++) {
+  cells[i].addEventListener("mouseover", () => {
+    if (mouseDown) {
+      cells[i].style.backgroundColor = "red";
+    }
+  });
+  
+  cells[i].addEventListener(
+    "click",
+    () => (cells[i].style.backgroundColor = "red")
+  );
 }
-
-//256 cells
